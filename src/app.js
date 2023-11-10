472fd5ef0ec0604ad7d3f8d8e13bf48a245c40e0
leo langlois <leo.langlois2@gmail.com>
2023-11-10 17:42:41 +0100

    add js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    console.log("ok")
+document.addEventListener("DOMContentLoaded",(e)=>{
+    //console.dir(document.children)
 
-    const version = 1
-    let bool = true
-    let double = 1.5
-    let nombre = 10
+    const version = Number(14) //type number
+    let bool = true //type float
+    let double = 1.5 //type number
+    let speack = "Hello" //type string
+    let type = null //type object
+    let nombre = 10; //type indéfinie
+    let table = ["html",1984, false]
+    let x = "10"
+    /* console.log(typeof table)
+    console.log(parseFloat(double)) */
 
-    nombre %2 ? console.log(`chiffre paire ${nombre}`) :
-    console.log(`chiffre impaire ${nombre}`)
+    const printout = `${speack} ${version}` // template scripting
+    console.log(printout)
+    //condition ternaire
+
+    nombre %2 === 0 ? console.log(`chiffre paire ${nombre}`) : console.log(`chiffre impaire ${nombre}`)
+
+    nombre === x ? console.log(true) : console.log(false)
+    console.table(table)
+    console.log(speack.split(""))
+
+    //tableaux
+    table.push(nombre)
+
+    const user = {
+        nom: "Javascript",
+        version: "1.6"
+    }
+    console.log(user.nom)
+    for (let index in user) {
+        let valeur = user.{index}
+        console.let("${index} : ${valeur}")
+    }
 })
