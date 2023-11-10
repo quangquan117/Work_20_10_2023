dd39a042811a1bbc50d7dcc87e245be5e8823904
leo langlois <leo.langlois2@gmail.com>
2023-11-10 14:52:07 +0100

    [Modif] Index.html (opengraph)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("ok")
+
+    const version = 1
+    let bool = true
+    let double = 1.5
+    let nombre = 10
+
+    nombre %2 ? console.log(`chiffre paire ${nombre}`) :
+    console.log(`chiffre impaire ${nombre}`)
 })
